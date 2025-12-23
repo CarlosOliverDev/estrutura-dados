b8dca11e9a49b8a3e46b9737886d63b29cd6ad49
Carlos <carlos.oli.h23@gmail.com>
2025-12-23 09:52:31 -0300

    Pilha funcionando.

---
+++ src/application/Programa.java
@@ -0,0 +1,45 @@
+package application;
+
+import entities.vetores.PilhaVetor;
+
+public class Programa {
+    public static void main(String[] args) {
+        int tamanhoPilha = 5;
+
+        PilhaVetor pilha = new PilhaVetor(tamanhoPilha);
+
+        pilha.imprimirTopo();
+        pilha.imprimirQuantidade();
+        pilha.exibirObjetos();
+
+        System.out.println();
+
+        pilha.empilhar("Carlos");
+        pilha.empilhar("Oliveira");
+        pilha.empilhar(22);
+        pilha.empilhar(1.65);
+        pilha.empilhar(false);
+        pilha.empilhar(true);
+        pilha.empilhar(false);
+        pilha.empilhar(98);
+
+        System.out.println();
+
+        pilha.imprimirTopo();
+        pilha.imprimirQuantidade();
+        pilha.exibirObjetos();
+
+        System.out.println();
+
+        pilha.desempilhar();
+        pilha.desempilhar();
+        pilha.desempilhar();
+
+        System.out.println();
+
+        pilha.imprimirTopo();
+        pilha.imprimirQuantidade();
+        pilha.exibirObjetos();
+
+    }
+}
--- src/entities/vetores/PilhaVetor.java
+++ src/entities/vetores/PilhaVetor.java
@@ -5,7 +5,7 @@ public class PilhaVetor {
     private Object[] vetor;
     private int topo = 0;
 
-    PilhaVetor(int tamanho_maximo) {
+    public PilhaVetor(int tamanho_maximo) {
         this.vetor = new Object[tamanho_maximo];
     }
 
@@ -19,45 +19,46 @@ public class PilhaVetor {
     public void empilhar(Object novoObjeto) {
         if(!isFull()) {
             vetor[topo] = novoObjeto;
+            System.out.println("Objeto " + novoObjeto + " empilhado.");
             topo++;
         } else {
-            System.out.println("\nA pilha está cheia, desempilhe um objeto antes de empilhar novamente.");
+            System.out.println("A pilha está cheia, desempilhe um objeto antes de empilhar novamente.");
         }
     }
 
     public void desempilhar() {
         if(!isEmpty()) {
-            System.out.println("\nObjeto " + vetor[topo] + " desempilhado.");
-            vetor[topo] = null;
             topo--;
+            System.out.println("Objeto " + vetor[topo] + " desempilhado.");
+            vetor[topo] = null;
         } else {
-            System.out.println("\nA pilha está vazia, empilhe um objeto para desempilhar.");
+            System.out.println("A pilha está vazia, empilhe um objeto para desempilhar.");
         }
     }
 
     public void imprimirTopo() {
         if(!isEmpty()) {
-            System.out.println("\nO objeto no topo é " + vetor[topo]);
+            System.out.println("O objeto no topo é " + vetor[topo-1]);
         } else {
-            System.out.println("\nA pilha está vazia.");
+            System.out.println("A pilha está vazia.");
         }
     }
 
     public void imprimirQuantidade() {
         if(isEmpty()) {
-            System.out.println("\nA pilha está vazia.");
+            System.out.println("A pilha está vazia.");
         } else {
-            System.out.printf("\nA pilha tem %d objetos.",topo);
+            System.out.printf("A pilha tem %d objetos.\n",topo);
         }
     }
 
     public void exibirObjetos() {
         if(isEmpty()) {
-            System.out.println("\nA pilha está vazia.");
+            System.out.println("A pilha está vazia.");
         } else {
             for(int i =0; i < vetor.length; i++) {
                 if(vetor[i] != null) {
-                    System.out.println("\n" + vetor[i]);
+                    System.out.println(vetor[i]);
                 }
             }
         }
