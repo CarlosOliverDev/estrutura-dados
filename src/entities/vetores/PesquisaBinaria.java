e4134cc053427a714dc70bee9b71a10c4a3b8c17
Carlos <carlos.oli.h23@gmail.com>
2026-01-08 15:59:46 -0300

    Estrutura feita.

--- src/entities/vetores/PesquisaBinaria.java
+++ src/entities/vetores/PesquisaBinaria.java
@@ -1,4 +1,25 @@
 package entities.vetores;
 
+import java.util.Scanner;
+
 public class PesquisaBinaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] intArray = new int[]{10,0,-2,32,41};
+
+        System.out.print("Digite qual valor você quer buscar: ");
+        int item = scanner.nextInt();
+        scanner.nextLine();
+        
+        organizarArray(intArray);
+
+        imprimirArray(intArray);
+
+        buscaBinaria(item, intArray);
+
+        imprimirArray(intArray);
+        
+        scanner.close();
+    }
+    
 }
