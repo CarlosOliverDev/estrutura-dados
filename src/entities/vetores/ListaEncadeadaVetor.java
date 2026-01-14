97167d7420ca534ec863cc0a3e845563caa6a8c7
Carlos <carlos.oli.h23@gmail.com>
2026-01-14 12:10:19 -0300

    Método de adicionar elemento ao meio adicionado.

--- src/entities/vetores/ListaEncadeadaVetor.java
+++ src/entities/vetores/ListaEncadeadaVetor.java
@@ -13,6 +13,28 @@ public class ListaEncadeadaVetor {
         cabeca = novo;
     }
 
+    public void addMeio(int valor, int posicao) {
+        if(posicao < 0) {
+            System.out.println("Posição inválida. Tente uma posição maior que 0.");
+            return;
+        } else if(posicao == 0) {
+            addInicio(valor);
+            return;
+        }
+
+        int contador = 1;
+        No novo = new No(valor);
+        No atual = cabeca;
+
+        while(atual.proximo != null && contador < posicao) {
+            atual = atual.proximo;
+            contador++;
+        }
+
+        novo.proximo = atual.proximo;
+        atual.proximo = novo;
+    }
+
     public void imprimirLista() {
         No atual = cabeca;
 
