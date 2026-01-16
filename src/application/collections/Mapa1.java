4c2d49c51551fb7b17b9fa197ff8ff762d50d7ce
Carlos <carlos.oli.h23@gmail.com>
2026-01-16 16:04:38 -0300

    Início implementação de Maps.

---
+++ src/application/collections/Mapa1.java
@@ -0,0 +1,12 @@
+package application.collections;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Mapa1 {
+    public static void main(String[] args) {
+
+        Map<String, String> capitais = new HashMap<>();
+
+    }
+}
