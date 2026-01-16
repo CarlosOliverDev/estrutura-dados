318a1e29921936e0a49ffaf65df4b7f62deb7817
Carlos <carlos.oli.h23@gmail.com>
2026-01-16 16:17:07 -0300

    Implementação da interface Map com a classe HashMap.

--- src/application/collections/Mapa1.java
+++ src/application/collections/Mapa1.java
@@ -5,8 +5,43 @@ import java.util.HashMap;
 
 public class Mapa1 {
     public static void main(String[] args) {
+        //Uso de Chave-Valor, sem se importar com ordenação.
 
+        //Declaração
         Map<String, String> capitais = new HashMap<>();
 
+        //Adicionando elementos
+        capitais.put("Brasil", "São Paulo");
+        capitais.put("País inexistente", "Brasília");
+        capitais.put("Brasil", "Brasília");
+        capitais.put("Inglaterra", "Londres");
+
+        System.out.println(capitais);
+
+        //Remover elemento
+        capitais.remove("País inexistente");
+
+        //Acessar elemento com key
+        System.out.println(capitais.get("Inglaterra"));
+
+        //Conferir chave ou valor
+        System.out.println(capitais.containsKey("Brasil"));
+        System.out.println(capitais.containsValue("Tokyo"));
+
+        capitais.put("Coreia do Sul", "Seul");
+        capitais.put("Japão", "Tokyo");
+        capitais.put("Peru", "Lima");
+
+        //Uso do for each
+        System.out.println("\nCapitais:");
+        for(Map.Entry<String, String> capital:capitais.entrySet()) {
+            System.out.println("País: " + capital.getKey());
+            System.out.println("Capital: " + capital.getValue() + "\n");
+        }
+
+        System.out.println(capitais.size());
+        System.out.println(capitais.values());
+
+        capitais.clear();
     }
 }
