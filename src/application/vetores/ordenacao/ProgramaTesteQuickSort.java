6b43f99ce88d339ad887413380805bb5e3014671
Carlos <carlos.oli.h23@gmail.com>
2026-01-19 16:15:41 -0300

    Revisão Quick Sort.

--- src/application/vetores/ordenacao/ProgramaTesteQuickSort.java
+++ src/application/vetores/ordenacao/ProgramaTesteQuickSort.java
@@ -4,7 +4,7 @@ import entities.sort.QuickSort;
 
 public class ProgramaTesteQuickSort {
     public static void main(String[] args) {
-        int[] array = {10,14,24,2,5,19,3,83,123,43,1,0,-2};
+        int[] array = {10,14,24,2,5,19,3,83,43,-2};
         new QuickSort(array);
         QuickSort.imprimirArray(array);
     }
--- src/entities/sort/QuickSort.java
+++ src/entities/sort/QuickSort.java
@@ -6,32 +6,26 @@ public class QuickSort {
     }
 
     public void quickSort(int[] array, int inicio, int fim) {
-        if(fim > inicio) {
-            int pivot = particionar(array, inicio, fim);
-
-            quickSort(array, inicio, pivot-1);
-            quickSort(array, pivot+1, fim);
+        if(inicio < fim) {
+            int pivo = particionar(array, inicio, fim);
+            quickSort(array, inicio, pivo-1);
+            quickSort(array, pivo+1, fim);
         }
     }
-
-    public int particionar(int[] array, int inicio, int fim) {
-        int pivot = array[fim];
+    public int particionar(int[]array, int inicio, int fim) {
+        int pivo = array[fim];
         int i = inicio;
-
-        for (int j = inicio; j < fim; j++) {
-            if(array[j] <= pivot) {
+        for(int j = inicio; j < fim; j++) {
+            if(array[j] <= pivo) {
                 int aux = array[i];
                 array[i] = array[j];
                 array[j] = aux;
-
                 i++;
             }
         }
-
         int aux = array[i];
         array[i] = array[fim];
         array[fim] = aux;
-
         return i;
     }
 
