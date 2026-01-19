faea19f4464ca20d267f88c89710a8ff2690e602
Carlos <carlos.oli.h23@gmail.com>
2026-01-19 13:48:25 -0300

    Selection Sort feito.

---
+++ src/application/vetores/ordenacao/ProgramaTesteSelection.java
@@ -0,0 +1,8 @@
+package application.vetores.ordenacao;
+
+public class ProgramaTesteSelection {
+    public static void main(String[] args) {
+        int[] arrayInt = new int[]{10, 98, 34, 7, 2, 19};
+        new SelectionSort(arrayInt);
+    }
+}
---
+++ src/application/vetores/ordenacao/SelectionSort.java
@@ -0,0 +1,27 @@
+package application.vetores.ordenacao;
+
+public class SelectionSort {
+    public SelectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minValue = i;
+
+            for (int j = i+1; j < array.length; j++) {
+                if(array[j] < array[minValue]) {
+                    minValue = j;
+                }
+            }
+
+            int aux = array[minValue];
+            array[minValue] = array[i];
+            array[i] = aux;
+        }
+
+        impressaoArray(array);
+    }
+
+    public void impressaoArray(int[] array) {
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+}
