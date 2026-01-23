783b9de017f95151670314eec1da0e4751a1c1e1
Carlos <carlos.oli.h23@gmail.com>
2026-01-23 20:01:29 -0300

    Adicionado método de gerar array aleatório e calculado tempo de organizar array com InsertionSort.

--- src/application/vetores/ordenacao/ProgramaInsertionSort.java
+++ src/application/vetores/ordenacao/ProgramaInsertionSort.java
@@ -4,13 +4,25 @@ import entities.sort.InsertionSort;
 
 public class ProgramaInsertionSort {
     public static void main(String[] args) {
-        int[] array = {19,42,2,-4,20,81,6};
+        int[] arrayPequeno = {19,42,2,-4,20,81,6};
         System.out.println("- InsertionSort -\nArray Desorganizado: ");
-        impressaoArray(array);
+        impressaoArray(arrayPequeno);
 
-        new InsertionSort(array);
-        System.out.println("\nArray Organizado: ");
-        impressaoArray(array);
+        long tempoInicialArrayPequeno = System.nanoTime();
+        new InsertionSort(arrayPequeno);
+        long tempoFinalArrayPequeno = System.nanoTime();
+
+        System.out.println("Array Organizado: ");
+        impressaoArray(arrayPequeno);
+        System.out.println("\nTempo para Organizar o Array: "+((tempoFinalArrayPequeno-tempoInicialArrayPequeno)/1_000_000) + "ms.");
+
+        int[] arrayGrande = gerarVetorAleatorio(10000);
+
+        long tempoInicialarrayGrande = System.nanoTime();
+        new InsertionSort(arrayGrande);
+        long tempoFinalarrayGrande = System.nanoTime();
+
+        System.out.println("\nTempo para organizar um Array de Tamanho 10.000: "+((tempoFinalarrayGrande-tempoInicialarrayGrande)/1_000_000) + "ms.");
     }
 
     public static void impressaoArray(int[] array) {
@@ -19,4 +31,13 @@ public class ProgramaInsertionSort {
         }
         System.out.println();
     }
+
+    public static int[] gerarVetorAleatorio(int tamanho) {
+        int[] vetor = new int[tamanho];
+        for (int i = 0; i < tamanho; i++) {
+            vetor[i] = (int) (Math.random() * 10000);
+        }
+        return vetor;
+    }
 }
+
